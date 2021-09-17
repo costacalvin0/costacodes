bab9f3cca42b57669e3026793bc86e8a24e5325c
Calvin Costa <costacalvin0@gmail.com>
2021-09-16 22:31:05 -0400

    sticky header + some fixes

---
+++ my-app/src/components/projects/card/index.tsx
@@ -0,0 +1,17 @@
+import { Card, CardActionArea, CardContent, CardHeader, CardMedia } from '@material-ui/core';
+import React from 'react'
+
+const ProjectCard = () => {
+    return (
+        <div>
+            <Card>
+                <CardMedia></CardMedia>
+                <CardHeader></CardHeader>
+                <CardContent></CardContent>
+                <CardActionArea></CardActionArea>
+            </Card>
+        </div>
+    )
+}
+
+export default ProjectCard;
